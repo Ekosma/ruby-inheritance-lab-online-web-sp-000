a0f38a5b165997c0ddc52b02b21fd4a76fc5b1e7
Evelyn Kosma <ezranaja@gmail.com>
2021-03-24 01:40:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initalize(knowledge)
+    @knowledge = []
+  end
+
+  def learn(things) 
+    @knowledge << things
+  end
 
 end
